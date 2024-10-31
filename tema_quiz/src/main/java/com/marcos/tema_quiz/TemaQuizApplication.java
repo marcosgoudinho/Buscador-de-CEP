018350dcb00d183f1560b3ee821ff6219266e68d
Marcos Vinicius Goudinho da Silva <167702986+marcosgoudinho@users.noreply.github.com>
2024-10-30 21:25:59 -0300

    [TRANS]: Transferir projeto do GitLab para o GitHub

---
+++ tema_quiz/src/main/java/com/marcos/tema_quiz/TemaQuizApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.tema_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemaQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemaQuizApplication.class, args);
+	}
+
+}
